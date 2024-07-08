3869772c5db061a091d535038e1a3b567dac7ac1
Semah Öncebe <75625270+semah702@users.noreply.github.com>
2024-07-08 03:00:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function closeNote() {
   document.getElementById("note-modal").style.display = "none";
 }
 
-// Pencere dışında tıklanınca modal'ı kapatma işlevi
 window.onclick = function(event) {
   var modal = document.getElementById("note-modal");
   if (event.target == modal) {
